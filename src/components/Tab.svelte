7f9115c750ff70b78b1c42795b5e117828b29348
Alejandro Díaz <alexvkcr@gmail.com>
2021-03-09 09:16:30 +0100

    feat/tabs distribution and fashion mode tab switch

--- src/components/Tab.svelte
+++ src/components/Tab.svelte
@@ -9,4 +9,15 @@
     display: inline-block;
     margin: 0 10px;
   }
+
+  @media (max-width: 600px) {
+    h1 {
+      margin: 20px 10px;
+    }
+  }
+  @media (max-width: 800px) {
+    h1 {
+      margin: 25px 10px;
+    }
+  }
 </style>
--- src/components/Tabs.svelte
+++ src/components/Tabs.svelte
@@ -7,11 +7,22 @@
   <Tab tabName="My CV" />
   <Tab tabName="My Projects" />
   <Tab tabName="Contact me" />
+  <Tab class="last-item" tabName="Fashion mode" />
 </div>
 
 <style>
   .tabs {
-    width: 100%;
+    width: calc(100% - 20px);
     height: 50px;
+    margin: 20px;
+
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    align-items: baseline;
+  }
+  .last-item {
+    order: 4; /* default is 0 */
+    color: red;
   }
 </style>
