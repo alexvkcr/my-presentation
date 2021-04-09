20e1f72e2bac7ae9386601d0d43d26cd9d0ef641
Alejandro Díaz <alexvkcr@gmail.com>
2021-04-09 16:00:39 +0200

    feat/delayedAppear component finished

---
+++ src/components/DelayedAppear.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let waitInMili = 500;
+  let shown = false;
+
+  onMount(() => setTimeout(() => (shown = true), waitInMili));
+</script>
+
+{#if shown}
+  <slot />
+{/if}
+
+<style></style>
