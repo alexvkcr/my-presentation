6dc43afbeca5df3bacad2cac3064f6ba3dbf2e91
Alejandro Díaz <alexvkcr@gmail.com>
2021-04-19 14:28:45 +0200

    feat/ocean comp added

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
   import Tabs from "./components/Tabs.svelte";
   import Error from "./components/Error.svelte";
   import Delayed from "./components/Delayed.svelte";
+  import Ocean from "./components/Ocean.svelte";
 
   const routes = {
     "/": Tabs,
@@ -12,6 +13,9 @@
 </script>
 
 <Router {routes} />
+<Delayed waitInMili={2500}>
+  <Ocean />
+</Delayed>
 
 <style>
 </style>
